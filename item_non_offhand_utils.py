c1dc6f1c86b159f2f62504c7e06e0a70947a7c53
Huydzvaidai <256951896+Huydzvaidai@users.noreply.github.com>
2026-03-09 16:09:32 +0700

    update

--- item_non_offhand_utils.py
+++ item_non_offhand_utils.py
@@ -386,6 +386,40 @@ class Item_non_offhand_Util:
                             }
                         },
                         "loop": True
+                    },
+                    "animation.player.first_person.attack_rotation": {
+                        "loop": True,
+                        "bones": {
+                            "rightarm": {
+                                "position": [
+                                    "math.sin(variable.attack_time * 180.0) * -12.0",
+                                    "math.sin((1.0 - variable.attack_time) * (1.0 - variable.attack_time) * 200.0) * 8.0 - variable.attack_time * 10.0",
+                                    "math.sin(variable.attack_time * 150.0) * 3.0"
+                                ],
+                                "rotation": [
+                                    "math.sin((1.0 - variable.attack_time) * (1.0 - variable.attack_time) * 250.0) * -50.0",
+                                    "math.sin((1.0 - variable.attack_time) * (1.0 - variable.attack_time) * 250.0) * 80.0",
+                                    "math.sin((1.0 - variable.attack_time) * (1.0 - variable.attack_time) * 250.0) * -30.0"
+                                ]
+                            }
+                        }
+                    },
+                    "animation.player.first_person.vr_attack_rotation": {
+                        "loop": True,
+                        "bones": {
+                            "rightarm": {
+                                "position": [
+                                    "6.0 * math.sin(variable.attack_time * 130.0)",
+                                    "(math.sin((1.0 - variable.attack_time) * (1.0 - variable.attack_time) * 190.0) - 0.7) * 9.0 + 5.0",
+                                    "math.sin(variable.attack_time * 130.0) * 14.0"
+                                ],
+                                "rotation": [
+                                    "32.0 * math.sin(variable.attack_time * -170.0 - 45.0) * 1.4",
+                                    "15.0 * math.sin(variable.attack_time * 160.0)",
+                                    "28.0 * math.sin(variable.attack_time * 190.0 + 25.0) * 1.3"
+                                ]
+                            }
+                        }
                     }
                 }
             }
